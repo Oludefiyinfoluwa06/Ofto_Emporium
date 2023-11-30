f4fd734d33e3bbd419048076dae980ed232491f5
Timothy <oludefiyinfoluwa06@gmail.com>
2023-11-30 06:43:19 +0100

    Seller dashboard navbar complete

--- users/seller/index.php
+++ users/seller/index.php
@@ -17,7 +17,7 @@
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_array($result);
             $fullname = $row["fullname"];
-            $seller_email = $row["email"];
+            $business_name = $row["business_name"];
         }
     }
 ?>
@@ -46,9 +46,96 @@
             background-color: #f8f8f8;
             color: #333;
         }
+
+        nav {
+            display: flex;
+            align-items: center;
+            justify-content: space-between;
+            padding: 20px 30px;
+            box-shadow: 2px 2px 10px #ccc;
+        }
+
+        nav .right {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            gap: 1.2rem;
+        }
+
+        .profile-noti {
+            display: flex;
+            align-items: center;
+            justify-content: flex-end;
+            gap: 1.2rem;
+        }
+
+        .search-bar {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+        }
+
+        .search {
+            display: flex;
+            align-items: center;
+            justify-content: flex-start;
+            gap: 0.6rem;
+            border: 2px solid #555;
+            border-radius: 5px;
+            /* padding: 5px 0; */
+        }
+
+        .search input {
+            border: none;
+            background: none;
+            outline: none;
+            height: 100%;
+        }
+
+        .search i {
+            margin: 7px 3px;
+        }
+
+        .search-bar button {
+            padding: 7px 10px;
+            margin-left: 10px;
+            background: #003399;
+            border: none;
+            border-radius: 5px;
+            color: #fff;
+            text-transform: uppercase;
+            cursor: pointer;
+        }
+
+        i {
+            cursor: pointer;
+            font-size: 20px;
+        }
     </style>
 </head>
 <body>
-    <p>Welcome back, <?php echo $fullname ?>. Your email is <?php echo $seller_email ?></p>
+    <nav>
+        <div class="right">
+            <div class="logo">
+                <img src="../../assets/OFTO_Emporium1.png" alt="Logo" width="40">
+            </div>
+
+            <i class="fa fa-bars"></i>
+        </div>
+        <div class="search-bar">
+            <div class="search">
+                <i class="fa fa-search"></i>
+                <input type="text" placeholder="Search">
+            </div>
+            <button>Search</button>
+        </div>
+        <div class="profile-noti">
+            <i class="fa fa-bell"></i>
+            <div class="profile">
+                <i class="fa fa-user"></i>
+                <i class="fa fa-angle-down"></i>
+            </div>
+        </div>
+    </nav>
 </body>
 </html>
